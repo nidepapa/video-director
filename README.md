880ee020c4fa85e8c109924bc44c1e20b95942bf
zhaoyufan <zhaoyufan@bilibili.com>
2023-10-01 19:04:03 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# video-director
+# Vlog-Director
+
+An automate vlog/video script generator empowered by Metaphor and OpenAI.
+
+## Usage
+
+Download the package
+
+```bash
+python3 app.py
+```
+
+## API - getMetaphorSearch
+
+Will interpret user's natural language search to a metaphor query
+and return blog content of recent (3 months) related to input tourism attractions
+
+```python
+
+    def getMetaphorSearch(self):
+        USER_QUESTION = "What's the popular travelling blog only about Los Angeles?"
+        SYSTEM_MESSAGE = "You are a helpful assistant that generates search queiries based on user questions. Only generate one search query."
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": SYSTEM_MESSAGE},
+                {"role": "user", "content": USER_QUESTION},
+            ],
+        )
+
+        date_90_days_ago = datetime.now() - timedelta(days=90)
+        # Format the date as "YYYY-MM-DD"
+        formatted_date = date_90_days_ago.strftime("%Y-%m-%d")
+
+        query = completion.choices[0].message.content
+        search_response = self.metaphor.search(
+            query, use_autoprompt=True, start_published_date=formatted_date
+        )
+
+        return search_response.get_contents()
+```
+
+## API - vlogScriptGenerator
+
+Will generate a vlog script based on the very first blog content(for demo purpose)
+and return in a formatted string
+
+```python
+    def vlogScriptGenerator(self, search_result):
+        # choose the very first result as demo
+        first_content = search_result.contents[0]
+        SYSTEM_MESSAGE = "You are a helpful assistant that generates vlog script with multiple scenes based on blog " \
+                         "content."
+        ASSISTANT_MESSAGE = "some context"
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": SYSTEM_MESSAGE},
+                {"role": "user", "name": "example_user", "content": "help me to generate a random script with some "
+                                                                 "scenes"},
+                {"role": "system", "name": "example_assistant",
+                 "content": ASSISTANT_MESSAGE},
+                {"role": "user", "content": first_content.extract},
+            ],
+        )
+
+        result_script = completion.choices[0].message.content
+        return "Vlog script for {0}: {1}".format(first_content.title, result_script)
+```
+
+## Reference
+
+metaphor API: 
+
+https://github.com/metaphorsystems/metaphor-python 
+
+OpenAI API: 
+
+https://cookbook.openai.com/examples/how_to_format_inputs_to_chatgpt_models
+
+# Future work
+1. more arguments can be added to make this API more customized, so people can choose any locations they prefer
+2. database can be added to store user preference history for future analysis, like who can be the potential content maker for video platform like Youtube
---
+++ app.py
@@ -0,0 +1,80 @@
+import os
+import openai
+from metaphor_python import Metaphor
+from datetime import datetime, timedelta
+
+os.environ["OPENAI_API_KEY"] = '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'
+os.environ["METAPHOR_API_KEY"] = '3ba9f09f-34e0-493e-a6e2-835304c79916'
+
+
+class VideoDirector:
+    def __init__(self):
+        self.metaphor = Metaphor(os.getenv("METAPHOR_API_KEY"))
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    def getMetaphorSearch(self):
+        USER_QUESTION = "What's the popular travelling blog only about Los Angeles?"
+        SYSTEM_MESSAGE = "You are a helpful assistant that generates search queiries based on user questions. Only generate one search query."
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": SYSTEM_MESSAGE},
+                {"role": "user", "content": USER_QUESTION},
+            ],
+        )
+
+        date_90_days_ago = datetime.now() - timedelta(days=90)
+
+        # Format the date as "YYYY-MM-DD"
+        formatted_date = date_90_days_ago.strftime("%Y-%m-%d")
+
+        query = completion.choices[0].message.content
+        search_response = self.metaphor.search(
+            query, use_autoprompt=True, start_published_date=formatted_date
+        )
+
+        return search_response.get_contents()
+
+    def vlogScriptGenerator(self, search_result):
+        # choose the very first result as demo
+        first_content = search_result.contents[0]
+        SYSTEM_MESSAGE = "You are a helpful assistant that generates vlog script with multiple scenes based on blog " \
+                         "content."
+        ASSISTANT_MESSAGE = "[SCENE 1: Mount Rainier's Grandeur] Narrator (Voiceover): (Proudly) 'Ascending to 14," \
+                            "410 feet above sea level, Mount Rainier stands as an icon in the Washington landscape.' " \
+                            "[Visuals: Breathtaking aerial views of Mount Rainier] Narrator (Voiceover): 'An active " \
+                            "volcano, Mount Rainier is the most glaciated peak in the contiguous U.S.A., " \
+                            "spawning five major rivers.' [Visuals: Time-lapse shots of glaciers and rivers] Narrator " \
+                            "(Voiceover): 'Subalpine wildflower meadows ring the icy volcano while ancient forest " \
+                            "cloaks Mount Rainier’s lower slopes.' [Visuals: Vibrant wildflowers and lush forest] " \
+                            "Narrator (Voiceover): 'Wildlife abounds in the park’s ecosystems. A lifetime of " \
+                            "discovery awaits.' [Visuals: Wildlife, including deer, bears, and birds] [SCENE 2: " \
+                            "Exploring the Park Narrator (Voiceover): 'Now, let's explore the opportunities that " \
+                            "await you at Mount Rainier National Park. [Visual transition: A map of the park with " \
+                            "various icons highlighting key locations] [SCENE 3: Road Status] Narrator (Voiceover): " \
+                            "'First, let's check out the road status.' [Visuals: A map showing road conditions] " \
+                            "Narrator (Voiceover): 'Find out current road conditions, including access to Paradise " \
+                            "during the winter season.' [Visuals: Snowy roads and tire chains]"
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": SYSTEM_MESSAGE},
+                {"role": "user", "name": "example_user", "content": "help me to generate a random script with some "
+                                                                    "scenes"},
+                {"role": "system", "name": "example_assistant",
+                 "content": ASSISTANT_MESSAGE},
+                {"role": "user", "content": first_content.extract},
+            ],
+        )
+
+        result_script = completion.choices[0].message.content
+        return "Vlog script for {0}: {1}".format(first_content.title, result_script)
+
+
+if __name__ == "__main__":
+    director = VideoDirector()
+    query_content = director.getMetaphorSearch()
+    demo_script = director.vlogScriptGenerator(query_content)
+    print(demo_script)
